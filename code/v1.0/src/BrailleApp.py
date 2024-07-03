979cab6f8d224bd519ced0ab0dcf1a5bdfed2345
Matías Villarreal Cruz <matias.villarreal@epn.edu.ec>
2024-07-02 20:41:37 -0500

    Modo Oscuro - Primera version de teclado - Modificaciones Diccionario

--- code/v1.0/src/BrailleApp.py
+++ code/v1.0/src/BrailleApp.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 from BrailleTranslator import BrailleTranslator
 from BrailleImageGenerator import BrailleImageGenerator
+from BrailleKeyboard import BrailleKeyboard
 
 class BrailleApp:
     def __init__(self, root, translator, image_generator):
@@ -35,8 +36,8 @@ class BrailleApp:
         self.btn_brl_to_esp = tk.Button(root, text="Braille a Español", command=self.convertir_brl_a_esp, font=fuente_grande)
         self.btn_brl_to_esp.grid(row=2, column=1, padx=10, pady=10, sticky='ew')
 
-        self.btn_generar_senaletica = tk.Button(root, text="Generar Señalética Braille", command=self.generar_senaletica, font=fuente_grande)
-        self.btn_generar_senaletica.grid(row=3, column=0, padx=10, pady=10, sticky='ew')
+        self.btn_teclado_virtual = tk.Button(root, text="Teclado en Braile", command=self.teclado_virtual, font=fuente_grande)
+        self.btn_teclado_virtual.grid(row=3, column=0, padx=10, pady=10, sticky='ew')
 
         self.btn_imprimir_espejo = tk.Button(root, text="Imprimir en Espejo", command=self.imprimir_espejo, font=fuente_grande)
         self.btn_imprimir_espejo.grid(row=3, column=1, padx=10, pady=10, sticky='ew')
@@ -45,7 +46,7 @@ class BrailleApp:
         self.btn_copiar_esp = tk.Button(root, text="Copiar Texto Español", command=self.copiar_texto_esp, font=fuente_grande)
         self.btn_copiar_esp.grid(row=4, column=0, padx=10, pady=10, sticky='ew')
 
-        self.btn_copiar_brl = tk.Button(root, text="Copiar Texto Braille", command=self.copiar_texto_esp, font=fuente_grande)
+        self.btn_copiar_brl = tk.Button(root, text="Copiar Texto Braille", command=self.copiar_texto_brl, font=fuente_grande)
         self.btn_copiar_brl.grid(row=4, column=1, padx=10, pady=10, sticky='ew')
 
         # Configurar el ajuste de columnas y filas
@@ -64,6 +65,17 @@ class BrailleApp:
         texto_esp = self.translator.braille_a_texto(texto_brl)
         self.text_esp.delete("1.0", tk.END)
         self.text_esp.insert(tk.END, texto_esp)
+
+    def teclado_virtual(self):
+        self.teclado_window = tk.Toplevel(self.root)
+        self.teclado_window.title("Teclado de Braille")
+        self.teclado = BrailleKeyboard(self.teclado_window, self.on_braille_input)
+        self.teclado_window.mainloop()
+
+    def on_braille_input(self, braille_text):
+        current_text = self.text_brl.get("1.0", tk.END).strip()
+        self.text_brl.delete("1.0", tk.END)
+        self.text_brl.insert(tk.END, current_text + braille_text)
 
     def generar_senaletica(self):
         texto_esp = self.text_esp.get("1.0", tk.END).strip()
@@ -87,4 +99,14 @@ class BrailleApp:
 
         # Mostrar mensaje de confirmación
         messagebox.showinfo("Copiar Texto", "Texto copiado al portapapeles con éxito.")
-        
+
+    def copiar_texto_brl(self):
+        texto_brl = self.text_brl.get("1.0", tk.END).strip()
+
+        # Copiar texto en Braille al portapapeles
+        self.root.clipboard_clear()
+        self.root.clipboard_append(texto_brl)
+        self.root.update()
+
+        # Mostrar mensaje de confirmación
+        messagebox.showinfo("Copiar Texto", "Texto copiado al portapapeles con éxito.")
